10b9dd7d0190e88cbd24ff8fc26e0fdda5695ad4
Jean-André Santoni <jean.andre.santoni@gmail.com>
2013-10-10 14:43:54 +0200

    Remove cordova

--- js/AppAvailability.js
+++
@@ -1,9 +0,0 @@
-window.appavailability = function(str, callback) {
-	cordova.exec(
-		function(){callback(true);},
-		function(err){callback(false);},
-		"AppAvailability",
-		"appavailability",
-		[str]
-	);
-};
